72913350648d6f7c1fb1946d69cdb1c204bdb0ff
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-17 19:58:40 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,7 @@
 class Book
   attr_accessor :author, :page_count,
   attr_reader :title, :genre
-  
-  
-  def genre=()
-    
-  end
+
     
     
     
@@ -17,6 +13,12 @@ class Book
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
+  end
+  
+    def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
   end
 
 end
